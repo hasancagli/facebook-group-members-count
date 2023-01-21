f378e39694f83694e5a643523140e494b5f8914b
Hasan Cagli <53555551+hasancagli@users.noreply.github.com>
2023-01-22 00:37:00 +0300

    code refactoring

--- counting.py
+++ counting.py
@@ -3,30 +3,19 @@ import time
 
 browser = webdriver.Firefox()
 
+# To Login to Facebook
 time.sleep(80)
 
 browser.get("https://www.facebook.com/groups/<GROUPid>/members/")
-
 time.sleep(1)
 
 def takeAllNames() :
-
-    isimler =  []
-
+    names =  []
     list1 = []
-    
-    #network = browser.find_element_by_id("mynetwork-tab-icon")
-    #network.click()
-    #time.sleep(1)
-    #contacts = browser.find_element_by_class_name("mn-community-summary__sub-section")
-    #contacts.click()
-    #time.sleep(1)
 
     browser.get("https://www.facebook.com/groups/<GROUPid>/members/")
-
     time.sleep(5)
     
-
     lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
     match=False
     while(match==False):
@@ -37,16 +26,10 @@ def takeAllNames() :
             match=True
     time.sleep(5)
 
-    kisiler = browser.find_elements_by_class_name("uiProfileBlockContent")
-    for e in kisiler :
-        isimler.append(e.text)
-    print(isimler)
-    print(len(isimler))
+    people = browser.find_elements_by_class_name("uiProfileBlockContent")
+    for e in people :
+        names.append(e.text)
     
-    
-##    kisiler = browser.find_elements_by_xpath("//span[contains(@class, 'mn-connection-card__name')]") 
-##    for e in kisiler:
-##        isimler.append(e.text)
-##    print(isimler)
+    return names, len(names)
 
-takeAllNames()
+names, length = takeAllNames()
